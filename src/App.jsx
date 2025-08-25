d679847534fdb1715fd44fbbc64f462951eb0369
Walter <waltermoreirasilvajunior@gmail.com>
2025-08-25 19:59:01 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatBot from './ChatBot';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App;
